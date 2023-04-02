f746e2d6f104259f9cad7142bc3dc50edf995704
audgks5551 <audgsk5551@daum.net>
2023-04-03 00:36:22 +0900

    init

---
+++ src/test/java/com/example/mission_leemyunghan/MissionLeeMyungHanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mission_leemyunghan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissionLeeMyungHanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
